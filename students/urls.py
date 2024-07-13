4932ccaa5279d75751f349060a493211f4a92900
shivam <sm9215076@gmail.com>
2024-07-13 10:21:39 +0530

    added views for view

--- students/urls.py
+++ students/urls.py
@@ -2,5 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns=[
-    path('data/',views.index, name='index')
+    path('data/',views.index, name='index'),
+    path('<int:id>',views.view_student,name='view_student'),
+    path('add/',views.add,name='add')
 ]
--- students/views.py
+++ students/views.py
@@ -1,6 +1,42 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponseRedirect
 from .models import Student
+from .forms import StudentForm
+from django.urls import reverse
 # Create your views here.
 def index(request):
     return render(request, 'index.html',{'students':Student.objects.all()})
+
+def view_student(request,id):
+    student = Student.objects.get(pk=id)
+    return HttpResponseRedirect(reverse('index'))
+
+def add(request):
+  if request.method == 'POST':
+    form = StudentForm(request.POST)
+    if form.is_valid():
+      new_student_number = form.cleaned_data['students_number']
+      new_first_name = form.cleaned_data['first_name']
+      new_last_name = form.cleaned_data['last_name']
+      new_email = form.cleaned_data['email']
+      new_field_of_study = form.cleaned_data['field_of_study']
+      new_gpa = form.cleaned_data['gpa']
+
+      new_student = Student(
+        students_number=new_student_number,
+        first_name=new_first_name,
+        last_name=new_last_name,
+        email=new_email,
+        field_of_study=new_field_of_study,
+        gpa=new_gpa
+      )
+      new_student.save()
+      return render(request, 'add.html', {
+        'form': StudentForm(),
+        'success': True
+      })
+  else:
+    form = StudentForm()
+  return render(request, 'add.html', {
+    'form': StudentForm()
+  })            
